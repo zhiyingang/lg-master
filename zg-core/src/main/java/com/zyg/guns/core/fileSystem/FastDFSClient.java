50998d698c240c7ac61c7b795eb70a12109d37a5
zhiyingang <yg.zhi@haihangyun.com>
2018-04-13 13:27:00 +0800

    12

---
+++ zg-core/src/main/java/com/zyg/guns/core/fileSystem/FastDFSClient.java
@@ -0,0 +1,203 @@
+package com.zyg.guns.core.fileSystem;
+
+/**
+ * Created by yg.zhi on 2018/4/12.
+ */
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.Logger;
+import org.csource.common.NameValuePair;
+import org.csource.fastdfs.ClientGlobal;
+import org.csource.fastdfs.StorageClient1;
+import org.csource.fastdfs.StorageServer;
+import org.csource.fastdfs.TrackerClient;
+import org.csource.fastdfs.TrackerServer;
+
+/**
+ *
+ * @描述: FastDFS分布式文件系统操作客户端 .
+ */
+public class FastDFSClient {
+
+
+    private static StorageClient1 storageClient1 = null;
+
+    private static Logger logger = Logger.getLogger(FastDFSClient.class);
+
+    private static String path;
+
+    /**
+     * 只加载一次.
+     */
+    static {
+        try {
+            String classPath = new File(FastDFSClient.class.getResource("/").getFile()).getCanonicalPath();
+//            String CONF_FILENAME = classPath + File.separator +"conf"+File.separator+ "fdfs_client.conf";
+            String CONF_FILENAME = "D:\\code"+File.separator+ "fdfs_client.conf";
+            logger.info("=== CONF_FILENAME:" + CONF_FILENAME);
+            ClientGlobal.init(CONF_FILENAME);
+            TrackerClient trackerClient = new TrackerClient(ClientGlobal.g_tracker_group);
+            TrackerServer trackerServer = trackerClient.getConnection();
+            if (trackerServer == null) {
+                logger.error("getConnection return null");
+            }
+            StorageServer storageServer = trackerClient.getStoreStorage(trackerServer);
+            if (storageServer == null) {
+                logger.error("getStoreStorage return null");
+            }
+            storageClient1 = new StorageClient1(trackerServer, storageServer);
+            path = "http://"+trackerServer.getInetSocketAddress().getHostString()+":"+trackerServer.getInetSocketAddress().getPort()+"/";
+        } catch (Exception e) {
+            logger.error(e);
+        }
+    }
+
+    /**
+     *
+     *            文件
+     * @param fileName
+     *            文件名
+     * @return 返回Null则为失败
+     */
+    public static String uploadFile(InputStream fis, String fileName) {
+        try {
+            NameValuePair[] meta_list = null; // new NameValuePair[0];
+            byte[] file_buff = null;
+            if (fis != null) {
+                int len = fis.available();
+                file_buff = new byte[len];
+                fis.read(file_buff);
+            }
+
+            String fileid = storageClient1.upload_file1(file_buff, getFileExt(fileName), meta_list);
+            return fileid;
+        } catch (Exception ex) {
+            logger.error(ex);
+            return null;
+        }finally{
+            if (fis != null){
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    logger.error(e);
+                }
+            }
+        }
+    }
+
+
+    public static String getPath(){
+        return path;
+    }
+
+    /**
+     * 根据组名和远程文件名来删除一个文件
+     *
+     * @param groupName
+     *            例如 "group1" 如果不指定该值，默认为group1
+     * @param fileName
+     *            例如"M00/00/00/wKgxgk5HbLvfP86RAAAAChd9X1Y736.jpg"
+     * @return 0为成功，非0为失败，具体为错误代码
+     */
+    public static int deleteFile(String groupName, String fileName) {
+        try {
+            int result = storageClient1.delete_file(groupName == null ? "group1" : groupName, fileName);
+            return result;
+        } catch (Exception ex) {
+            logger.error(ex);
+            return 0;
+        }
+    }
+
+    /**
+     * 根据fileId来删除一个文件（我们现在用的就是这样的方式，上传文件时直接将fileId保存在了数据库中）
+     *
+     * @param fileId
+     *            file_id源码中的解释file_id the file id(including group name and filename);例如 group1/M00/00/00/ooYBAFM6MpmAHM91AAAEgdpiRC0012.xml
+     * @return 0为成功，非0为失败，具体为错误代码
+     */
+    public static int deleteFile(String fileId) {
+        try {
+            int result = storageClient1.delete_file1(fileId);
+            return result;
+        } catch (Exception ex) {
+            logger.error(ex);
+            return 0;
+        }
+    }
+
+    /**
+     * 修改一个已经存在的文件
+     *
+     * @param oldFileId
+     *            原来旧文件的fileId, file_id源码中的解释file_id the file id(including group name and filename);例如 group1/M00/00/00/ooYBAFM6MpmAHM91AAAEgdpiRC0012.xml
+     * @param file
+     *            新文件
+     * @param filePath
+     *            新文件路径
+     * @return 返回空则为失败
+     */
+    public static String modifyFile(String oldFileId, InputStream file, String filePath) {
+        String fileid = null;
+        try {
+            // 先上传
+            fileid = uploadFile(file, filePath);
+            if (fileid == null) {
+                return null;
+            }
+            // 再删除
+            int delResult = deleteFile(oldFileId);
+            if (delResult != 0) {
+                return null;
+            }
+        } catch (Exception ex) {
+            logger.error(ex);
+            return null;
+        }
+        return fileid;
+    }
+
+    /**
+     * 文件下载
+     *
+     * @param fileId
+     * @return 返回一个流
+     */
+    public static InputStream downloadFile(String fileId) {
+        try {
+            byte[] bytes = storageClient1.download_file1(fileId);
+            InputStream inputStream = new ByteArrayInputStream(bytes);
+            return inputStream;
+        } catch (Exception ex) {
+            logger.error(ex);
+            return null;
+        }
+    }
+
+    /**
+     * 获取文件后缀名（不带点）.
+     *
+     * @return 如："jpg" or "".
+     */
+    private static String getFileExt(String fileName) {
+        if (StringUtils.isBlank(fileName) || !fileName.contains(".")) {
+            return "";
+        } else {
+            return fileName.substring(fileName.lastIndexOf(".") + 1); // 不带最后的点
+        }
+    }
+
+
+    public static void main(String[] args) throws IOException{
+        File file = new File("D:\\code\\1.jpg");
+
+        String a = uploadFile(new FileInputStream(file),file.getName());
+        System.out.println(a);
+        System.out.println(getPath());
+    }
+}
